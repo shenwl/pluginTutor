3e22d1d2d557c160ab939ed2b715192ff30d1576
shenwl <shenweilun96@gmail.com>
2020-04-14 22:14:28 +0800

    测试emit hook

--- plugins/copyright-webpack-plugin.js
+++ plugins/copyright-webpack-plugin.js
@@ -4,8 +4,15 @@ class CopyrightWebpackPlugin {
   }
 
   // compiler可以理解为webpack的一个实例
+  // 打包所有相关内容都在compiler存放
   apply(compiler) {
-    console.log(compiler.hooks);
+    // emit hook
+    // @see https://webpack.js.org/api/compiler-hooks/#emit
+    // compilation存放了和这次打包相关的内容
+    compiler.hooks.emit.tapAsync('CopyrightWebpackPlugin', (compilation, cb) => {
+      console.log('-------emit hook------');
+      cb();
+    });
   }
 }
 
